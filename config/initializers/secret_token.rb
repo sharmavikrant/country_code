3a9b7eabf1b6c7380ed95fea9732827a5094664d
Vikrant <vikrant.sharma@travletriangle.com>
2015-10-17 23:26:41 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountryCheck::Application.config.secret_token = '2161ebf0a757cefa7d09b1f94c4cc6fda73e579f25e278eab1fc49e41178436a2869c29dc334711e8a2b46fbdc4e0c35a6a270a67eab15350ebe30c5dc407fbd'
